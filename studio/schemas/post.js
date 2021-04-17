220dabaec8b9eef7ad30836bc1781d926fb6c8f5
Patrick Simonsen <tb.patrick.s@gmail.com>
2021-04-17 13:43:40 +0200

    Getting started

--- studio/schemas/post.js
+++ studio/schemas/post.js
@@ -18,12 +18,6 @@ export default {
       }
     },
     {
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'}
-    },
-    {
       name: 'mainImage',
       title: 'Main image',
       type: 'image',
